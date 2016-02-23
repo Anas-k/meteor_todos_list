20a8f36063cd8705ceb51726d6f3081c41aded1b
Anas-k <khtib.anas@gmail.com>
2016-02-23 21:18:42 +0100

    add inpute ype in html + createdAt in js

--- simple-todos.js
+++ simple-todos.js
@@ -12,10 +12,45 @@ if (Meteor.isClient) {
 
     tasks: function () {
 
-      return Tasks.find({});
+      return Tasks.find({}, {sort: {createdAt: -1} });
 
     }
 
   });
+  Template.body.events({
+
+  "submit .new-task": function (event) {
+
+    // Prevent default browser form submit
+
+    event.preventDefault();
+
+
+
+    // Get value from form element
+
+    var text = event.target.text.value;
+
+
+
+    // Insert a task into the collection
+
+    Tasks.insert({
+
+      text: text,
+
+      createdAt: new Date() // current time
+
+    });
+
+
+
+    // Clear form
+
+    event.target.text.value = "";
+
+  }
+
+});
 
 }
